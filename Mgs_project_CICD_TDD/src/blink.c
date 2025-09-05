04bd071dc7d199fe787683e4d45caf8dd543627e
Paweł Ozga <ozgaapawell@gmail.com>
2025-09-05 19:39:09 +0200

    refresh .uf2

--- Mgs_project_CICD_TDD/src/blink.c
+++ Mgs_project_CICD_TDD/src/blink.c
@@ -1,6 +1,6 @@
 #include "pico/stdlib.h"
 int main() {
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    const uint LED_PIN = 25; //PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
