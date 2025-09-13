b1588832ef29ecf6d802644fac6bd8d898123a76
Paweł Ozga <ozgaapawell@gmail.com>
2025-09-13 19:31:43 +0200

    updated CMakeLists.txt, blink.c - added info about cmake -DPICO_BOARD=pico2 ..

--- Mgs_project_CICD_TDD/src/blink.c
+++ Mgs_project_CICD_TDD/src/blink.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 int main() {
-    const uint LED_PIN = 25; //PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    const uint GPIO2 = 4; //PICO_DEFAULT_LED_PIN;
+    gpio_init(GPIO2;
+    gpio_set_dir(GPIO2, GPIO_OUT);
     stdio_init_all();
     while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
+        gpio_put(GPIO2, 1);
         sleep_ms(250);
         printf("Blinking\r\n");
+        gpio_put(GPIO2, 0);
+        sleep_ms(250);
+        printf("OFF\r\n");
     }
 }
