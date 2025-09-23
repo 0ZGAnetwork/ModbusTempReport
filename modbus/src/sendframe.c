d42291f29248715f180be6718d9f76f5ade01b59
Paweł Ozga <ozgaapawell@gmail.com>
2025-09-23 22:42:57 +0200

    add sendframe.c to test if program works correct in workflow

--- modbus/src/sendframe.c
+++ modbus/src/sendframe.c
@@ -1,3 +1,20 @@
 #include <pico/stdlib.h>
 #include <hardware/uart.h>
+#include <stdio.h>
 
+// test for modbus project
+int main() {
+    const uint GPIO2 = 2;
+    gpio_init(GPIO2);
+    gpio_set_dir(GPIO2, GPIO_OUT);
+    stdio_init_all();
+    while (true) {
+        gpio_put(GPIO2, 1);
+        sleep_ms(250);
+        printf("Modbus file test: Blink\r\n");
+        gpio_put(GPIO2, 0);
+        sleep_ms(250);
+        printf("Modbus file test: OFF\r\n");
+    }
+}
+// only works with C11 standard!
