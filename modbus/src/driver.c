bb908dd3d6249467d625b84fa6d40d94e06b0e27
Paweł Ozga <ozgaapawell@gmail.com>
2025-09-26 05:25:16 +0200

    add .json file for Mgs_project and modbus, update src in modbus

--- modbus/src/driver.c
+++ modbus/src/driver.c
@@ -2,6 +2,8 @@
 #include "pico/time.h"
 #include <string.h>
 
+#define TIMEOUT_US 200000
+
 void uart_init_max485() {
     uart_init(UART_PORT, 9600);
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
@@ -12,33 +14,38 @@ void uart_init_max485() {
     uart_set_format(UART_PORT, 8, 1, UART_PARITY_NONE);
 }
 
-int read_modbus_register(int slave_addr, int reg_addr, int *value) {
+int read_modbus_register(int slave_addr, int reg_addr, int num_regs,  int *values) {
     // Implementation of reading a Modbus register
     unsigned char frame[8];
-    unsigned char response[16];
-    int len;
 
     frame[0] = (unsigned char)slave_addr;
     frame[1] = 0x03;               // Read Holding Register
     frame[2] = (reg_addr >> 8) & 0xFF;
     frame[3] = reg_addr & 0xFF;
-    frame[4] = 0x00;
-    frame[5] = 0x01;
+    frame[4] = (num_regs >> 8) & 0xFF;;
+    frame[5] = num_regs & 0xFF;
 
     unsigned short crc = crc16_modbus(frame, 6);
     frame[6] = crc & 0xFF;
     frame[7] = (crc >> 8) & 0xFF;
 
     uart_send(frame, 8);
-    len = uart_receive(response, 7);
-    if (len < 7) return -1;
 
-    unsigned short crc_resp = crc16_modbus(response, len - 2);
-    unsigned short crc_recv = response[len - 2] | (response[len - 1] << 8);
+    //dynamic wait time based on number of registers
+    unsigned char response[64];
+    int resp_len = 2 * num_regs + 5;
+    int rcv = uart_received_timeout(response, resp_len, TIMEOUT_US);
+    if (rcv < resp_len) return -1; //timeout
+
+    unsigned short crc_resp = crc16_modbus(response, resp_len - 2);
+    unsigned short crc_recv = response[resp_len - 2] | (response[resp_len - 1] << 8);
     if (crc_resp != crc_recv) return -2;  // invalid CRC
     if (response[1] != 0x03) return -3;   // error in response
 
-    *value = (response[3] << 8) | response[4];
+    for (int i = 0; i < num_regs; i++) {
+        values[i] = response[3 +2*i] << 8 | response[4 + 2*i];
+    }
+
     return 0;
 }
 
@@ -46,12 +53,24 @@ void uart_send(const unsigned char *data, int len) {
     // Implementation of UART send
     gpio_put(MAX485_DE_RE_PIN, 1);
     uart_write_blocking(UART_PORT, data, len);
+    sleep_us(50); // wait for transmission to complete for max485 board
     gpio_put(MAX485_DE_RE_PIN, 0);
 }
 
-int uart_receive(unsigned char *buf, int max_len) {
+int uart_received_timeout(unsigned char *buf, int expected_len,unsigned int timeout_us) {
     // Implementation of UART receive
-    return uart_read_blocking(UART_PORT, buf, max_len);
+    int received = 0;
+    absolute_time_t start = get_absolute_time();
+
+    while (received < expected_len) {
+        if (uart_is_readable(UART_PORT)) {
+            buf[received++] = uart_getc(UART_PORT);
+        }
+        if (absolute_time_diff_us(start, get_absolute_time()) > timeout_us) {
+            return -1; // timeout
+        }
+    }
+    return received;
 }
 
 unsigned short crc16_modbus(const unsigned char *buf, int len) {
@@ -59,7 +78,7 @@ unsigned short crc16_modbus(const unsigned char *buf, int len) {
     unsigned short crc = 0xFFFF;
     for (int pos = 0; pos < len; pos++) {
         crc ^= (unsigned short)buf[pos];
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 8; i++) 
             if (crc & 1)
                 crc = (crc >> 1) ^ 0xA001;
             else
--- modbus/src/driver_control.c
+++ modbus/src/driver_control.c
@@ -5,27 +5,20 @@
 #define SLAVE_ADDR 1
 
 void create_snapshot(SDC35Status *status) {
-    int value;
 
-    format_timestamp(status->timestamp, sizeof(status->timestamp));
+unsigned short regs[3];
+int start_addr = 0x1455;
+int num_regs = 3;
 
-    //read PV placeholder RAM Address)
-    if (read_modbus_register(SLAVE_ADDR, 0x1455, &value) == 0) { //PV Input Range Low Limit
-        status->pv = (float)value/10.0f; //scaling
-    } else {
-        status->pv = -1;
-    }
+int result = read_modbus_registers(SLAVE_ADDR, start_addr, num_regs, regs);
 
-    if (read_modbus_register(SLAVE_ADDR, 0x1457, &value) == 0) { //SV Input Range Low Limit
-        status->sv = (float)value/10.0f; //scaling
-    } else {
-        status->sv = -1;
-    }
-
-    if (read_modbus_register(SLAVE_ADDR, 0x23F1, &value) == 0) { //Alarm status
-        status->alarm = value; //scaling
-    } else {
-        status->alarm = -1;
-    }
-    
+if (result == 0) {
+    status->pv    = (float)regs[0] / 10.0f;
+    status->sv    = (float)regs[1] / 10.0f;
+    status->alarm = regs[2];
+} else {
+    status->pv = -1;
+    status->sv = -1;
+    status->alarm = -1;
 }
+
