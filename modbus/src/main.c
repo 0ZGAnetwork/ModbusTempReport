95882ab9c14e3aa9ad7f0b806e3904c4a4e0c0a1
Paweł Ozga <ozgaapawell@gmail.com>
2025-09-26 05:25:16 +0200

    add .json file for Mgs_project and modbus, update src in modbus

--- modbus/src/main.c
+++ modbus/src/main.c
@@ -1,8 +1,10 @@
 #include "sdc35_status.h"
 #include "driver_control.h"
 
+
 int main (void){
     stdio_init_all();
+    uart_init_max485();
     
     SDC35Status status;
     create_snapshot(&status);
