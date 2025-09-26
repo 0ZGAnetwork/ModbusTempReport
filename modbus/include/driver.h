37be9ca8724f8b7c3a99b8517edf49cfa5d9cb15
Paweł Ozga <ozgaapawell@gmail.com>
2025-09-26 16:27:58 +0200

    fix compilerparh in .vscode/setting.json, fix syntax in driver.c/.h etc

--- modbus/include/driver.h
+++ modbus/include/driver.h
@@ -1,9 +1,9 @@
-#ifdef DRIVER_H
+#ifndef DRIVER_H
 #define DRIVER_H
 
+#include "pico/stdlib.h"
 #include "hardware/uart.h"
 #include "hardware/gpio.h"
-#include "pico/stdlib.h"
 
 // --- UART/MAX485-SDC35 ---
 #define UART_PORT uart0
@@ -13,10 +13,9 @@
 #define MAX485_DE_RE_PIN 22
 
 // --- UART/modbus ---
-int read_modbus_register(int slave_addr, int reg_addr, int *value);
+int read_modbus_register(int slave_addr, int reg_addr, int num_regs,  int *values);
 void uart_send(const unsigned char *data, int len);
-int uart_receive(unsigned char *buf, int max_len);
+int uart_received_timeout(unsigned char *buf, int expected_len,unsigned int timeout_us);
 unsigned short crc16_modbus(const unsigned char *buf, int len);
-
 
 #endif
